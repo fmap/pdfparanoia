72b017075f2fccca3c9d568b72d10df5b65806c9
vi <noreply@example.org>
2013-12-08 14:48:45 +0800

    Tests for new JSTOR generator (#24)?

--- tests/test_jstor.py
+++ tests/test_jstor.py
@@ -5,16 +5,13 @@ import pdfparanoia
 
 class JSTORTestCase(unittest.TestCase):
     def test_jstor(self):
-        file_handler = open("tests/samples/jstor/231a515256115368c142f528cee7f727.pdf", "rb")
+        file_handler = open("tests/samples/jstor/0271404496b9cb5d981c10ea6aa27dd6.pdf", "rb")
         content = file_handler.read()
         file_handler.close()
-        self.assertIn("\n18 0 obj \n", content)
-
-        # this section will later be manipulated
-        self.assertIn("\n19 0 obj \n", content)
+        self.assertIn("\n19 0 obj\n", content)
 
         output = pdfparanoia.plugins.JSTOR.scrub(content)
 
         # FlateDecode should be replaced with a decompressed section
-        self.assertIn("\n19 0 obj\n<</Length 2862>>stream", output)
+        self.assertIn("\n19 0 obj\n<</Length 2788", output)
 
